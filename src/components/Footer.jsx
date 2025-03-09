d2160c8d6616cff5f8d7671134155cfe884c5aa7
Mohamed-Faiz-Khan <141932267+Mohamed-Faiz-Khan@users.noreply.github.com>
2025-03-09 16:37:32 +0530

    Delete src/components directory

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/Footer.css';
-
-function Footer() {
-  return <footer className="footer">© 2025 AuctionMaster. All Rights Reserved.</footer>;
-}
-
-export default Footer;
